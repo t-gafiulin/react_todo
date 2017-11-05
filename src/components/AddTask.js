432c17086dc1b4aaaaca2e8baa19b73a15d600cf
Timur Gafiulin <nem00700@gmail.com>
2017-11-05 22:04:25 +0300

    addd inclusion of the task in the task list

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,8 +4,27 @@ import React, { Component } from 'react';
 
 export default class AddTask extends Component{
 
+    constructor(){
+        super();
+        this.state = {
+            value: ''
+        }
+    }
+
+    handleChange(e){
+        this.setState({value: e.target.value});
+    }
+
+    handleClick(){
+        //this.props.addTask.bind(null, this.state.value);
+        this.props.addTask(this.state.value);
+        this.setState({value: ''});
+    }
 
     render(){
-        return <div>Add task</div>;
+        return <div>
+            <input value={this.state.value} onChange={this.handleChange.bind(this)} />
+            <button onClick={this.handleClick.bind(this)}>Add task</button>
+        </div>;
     }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,11 +4,23 @@ import Task from './Task';
 
 
 export default class TodoList extends Component{
+    constructor(){
+        super();
+        this.state = {
+            tasks: []
+        }
+    }
 
+    addTask(task){
+        let newTasks = this.state.tasks;
+        newTasks.push(task);
+        this.setState({tasks: newTasks});
+        console.log(this.state.tasks);
+    }
 
     render(){
         return <div>
-            <AddTask />
+            <AddTask addTask={this.addTask.bind(this)} />
             <Task />
             <Task />
         </div>;
