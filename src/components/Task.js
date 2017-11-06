8c0dc7de096c46a077c25054d84c27d0d1dcbcae
Timur Gafiulin <nem00700@gmail.com>
2017-11-06 12:20:23 +0300

    fix task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 
 export default class Task extends Component{
     render(){
-        return <div className={`task ${this.props.addClass}`}>
-            {this.props.task}
+        return <div className={`task-block ${this.props.addClass}`}>
+            <div className='task-block__task'>{this.props.task}</div>
             <i class="fa fa-trash" aria-hidden="true" onClick={this.props.deleteTask.bind(null, this.props.index)}></i>
         </div>;
     }
