76724ca157e9d2b1aec042e94dc3643f46e270a3
Timur Gafiulin <nem00700@gmail.com>
2017-11-06 20:20:35 +0300

    add create a new task by pressing Enter and delete add button

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -12,21 +12,24 @@ export default class AddTask extends Component{
     }
 
     handleChange(e){
-        this.setState({value: e.target.value});
-    }
-
-    handleClick(){
-        if(this.state.value !== ''){
-            this.props.addTask(this.state.value);
-            this.setState({value: ''});
+        if(e.key === 'Enter'){
+            if(this.state.value !== ''){
+                this.props.addTask(this.state.value);
+                this.setState({value: ''});
+            }
+        }else{
+            this.setState({value: this.state.value + e.key});
         }
     }
 
     render(){
         return <div className='add-task'>
             I need to
-            <input className='add-task__input' value={this.state.value} onChange={this.handleChange.bind(this)} />  
-            <i className="fa fa-plus" aria-hidden="true" onClick={this.handleClick.bind(this)}></i>
+            <input 
+                className='add-task__input' 
+                value={this.state.value} 
+                onKeyPress={this.handleChange.bind(this)} 
+            />  
         </div>;
     }
 }
